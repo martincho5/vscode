7b44458419f731b323b2d152f3ecc99db667b8b0
Martin Ferrini <martinferrini@gmail.com>
2019-07-12 17:51:56 -0300

    Actualizacion con Archivos para enviar por mail

--- kanbantool/apiconnect.py
+++ kanbantool/apiconnect.py
@@ -9,14 +9,14 @@ import requests
 #    print(p['name'],p['id'])
 #341513
 #/boards/:board_id/preload.json
-#tecnologia = requests.get('https://uadedesa.kanbantool.com/api/v3//boards/341513/preload.json?access_token=M2U263DDN63Y')
+tecnologia = requests.get('https://uadedesa.kanbantool.com/api/v3//boards/341513/preload.json?access_token=M2U263DDN63Y')
 #print(tecnologia.text)
-#tecnologia_json = tecnologia.json()
+tecnologia_json = tecnologia.json()
 #print(tecnologia_json)
 #for tec in tecnologia_json['swimlanes']:
 #   print(tec['name'])
-#for tec in tecnologia_json['workflow_stages']:
-#   print(tec['name'],tec['id'])
+for tec in tecnologia_json['workflow_stages']:
+   print(tec['name'],tec['id'])
 
 tecnologiatasks = requests.get('https://uadedesa.kanbantool.com/api/v3/boards/341513.json?access_token=M2U263DDN63Y')
 #print(tecnologia.text)
@@ -27,12 +27,27 @@ idTarea = '24863408'
 tarea2 = '.json?access_token=M2U263DDN63Y'
 reqpost = tareas + idTarea + tarea2
 print(reqpost)
-archivartarea = requests.post('https://uadedesa.kanbantool.com/api/v3/task/24863408.json?access_token=M2U263DDN63Y')
+fcompletados = open("completadas.txt","w")
+fpendientes = open("pendientes.txt","w")
+#archivartarea = requests.post('https://uadedesa.kanbantool.com/api/v3/task/24863408.json?access_token=M2U263DDN63Y')
 for tec in tecnologiatarea_json['tasks']:
    if tec['workflow_stage_id'] == 2584763:
+      #Trae las tareas que están en estado Completadas
+      print("Completadas")
       print(tec['name'],tec['id'],tec['workflow_stage_id'])
-      
-
+      fcompletados.write(tec['name']+"\n")
+   elif tec['workflow_stage_id'] == 2584765:
+      #Trae las tareas que están en estado Esperando
+      print("Esperando")
+      print(tec['name'],tec['id'],tec['workflow_stage_id'])
+      fpendientes.write(tec['name']+"\n")
+   elif tec['workflow_stage_id'] == 2584767:
+      #Trae las tareas que están en estado Trabajando
+      print("Esperando")
+      print(tec['name'],tec['id'],tec['workflow_stage_id'])
+      fpendientes.write(tec['name']+"\n")
+fcompletados.close()
+fpendientes.close()
 
 #request = requests.get('http://api.open-notify.org')
 #print(request.text)
