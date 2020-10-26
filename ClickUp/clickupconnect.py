c70d9455098c14df26c42951d9f57ebd2d0e0436
martincho5 <40181060+martincho5@users.noreply.github.com>
2020-10-26 14:06:31 -0300

    Update clickupconnect.py

--- ClickUp/clickupconnect.py
+++ ClickUp/clickupconnect.py
@@ -1,16 +1,16 @@
 import requests
 
-tecnologia = requests.get('https://api.clickup.com/api/v2/user', headers={'Authorization': 'pk_3006467_Q7XNHPB1A10NAQ6H4564VP6UGSLCW0EL'})
+tecnologia = requests.get('https://api.clickup.com/api/v2/user', headers={'Authorization': '######'})
 print(tecnologia.text)
 
 
-tecnologia2 = requests.get('https://api.clickup.com/api/v2/team', headers={'Authorization': 'pk_3006467_Q7XNHPB1A10NAQ6H4564VP6UGSLCW0EL'})
+tecnologia2 = requests.get('https://api.clickup.com/api/v2/team', headers={'Authorization': '######'})
 print(tecnologia2.text)
 
-tecnologia3 = requests.get('https://api.clickup.com/api/v2/team/3001554/space?archived=false', headers={'Authorization': 'pk_3006467_Q7XNHPB1A10NAQ6H4564VP6UGSLCW0EL'})
+tecnologia3 = requests.get('https://api.clickup.com/api/v2/team/3001554/space?archived=false', headers={'Authorization': '######'})
 print(tecnologia3.text)
 
-tecnologia4 = requests.get('https://api.clickup.com/api/v2/space/3011253/folder?archived=false', headers={'Authorization': 'pk_3006467_Q7XNHPB1A10NAQ6H4564VP6UGSLCW0EL'})
+tecnologia4 = requests.get('https://api.clickup.com/api/v2/space/3011253/folder?archived=false', headers={'Authorization': '#####'})
 print(tecnologia4.text)
 
 #https://api.clickup.com/api/v2/folder/folder_id/list?archived=false
