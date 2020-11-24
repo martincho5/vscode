0f9e5f5e28baf50b8b5a4321d438f2d25133867f
martincho5 <40181060+martincho5@users.noreply.github.com>
2020-11-24 16:32:58 -0300

    Ordenando los archivos de PowerShell

--- Powershell/Manejo de Archivos _ Files/BuscardentrodeArchivos.ps1
+++ Powershell/Manejo de Archivos _ Files/BuscardentrodeArchivos.ps1
@@ -3,6 +3,6 @@ Get-ChildItem -recurse | Select-String -pattern "UPLSave" | group path | select 
 Get-ChildItem -recurse | Select-String -pattern "if request" | Export-CSV c:\
 Get-ChildItem -recurse | Select-String -pattern "ASPX Shell by LT" | Export-CSV d:\
 Get-ChildItem -recurse | Select-String -pattern "186.138.18.37" 
-Get-ChildItem -recurse | Select-String -pattern "powershell" | Export-CSV d:\sabato.csv
-Get-ChildItem -recurse | Select-String -pattern "192.168.64" | Export-CSV d:\sabato.csv
+Get-ChildItem -recurse | Select-String -pattern "powershell" | Export-CSV d:\powerhsell.csv
+Get-ChildItem -recurse | Select-String -pattern "192.168.64" | Export-CSV d:\IP.csv
 
--- Powershell/Manejo de Archivos _ Files/BuscoPorinfodeArchivo.ps1
+++ Powershell/Manejo de Archivos _ Files/BuscoPorinfodeArchivo.ps1
@@ -1,8 +1,6 @@
-﻿Get-ChildItem c:\ -Recurse | ? { $_.Versioninfo.OriginalFilename -match "uade.exe|oemsetup.exe|startup.exe|psexec.c|admin" } | fl VersionInfo
+﻿# Buscar en los archivos por VersionInfo del archivo
+Get-ChildItem c:\ -Recurse | ? { $_.Versioninfo.OriginalFilename -match "uade.exe|oemsetup.exe|startup.exe|psexec.c|admin" } | fl VersionInfo
 Get-ChildItem d:\ -Recurse | ? { $_.Versioninfo.OriginalFilename -match "uade.exe|oemsetup.exe|startup.exe|psexec.c" } | fl VersionInfo
-
 Get-ChildItem D:\ -Recurse | ? { $_.Versioninfo.OriginalFilename -contains 'uade.exe' } | fl VersionInfo
-
 Get-ChildItem C:\Windows\notepad.exe | Format-List VersionInfo
-
 Get-ChildItem c:\windows\system32\sethc.exe | Format-List VersionInfo
